3a67a89d281e818a16bcb27c5f656a9f530d641e
byanezzz <biancayanez89@gmail.com>
2018-02-20 00:39:09 -0300

    consultas a API por filtros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-//Autentificación del usuario
+$('.dropdown-button').dropdown({
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: true, // Does not change width of dropdown to that of the activator
+  hover: false, // Activate on hover
+  gutter: 0, // Spacing from edge
+  belowOrigin: false, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+});
+
 //-------INICIALIZANDO Firebase------------------//
-var config = {
+/* var config = {
   apiKey: "AIzaSyBnXR6kFDHMqSuKkRpUjDVozxtoVaboOs0",
   authDomain: "ourapp-ea016.firebaseapp.com",
   databaseURL: "https://ourapp-ea016.firebaseio.com",
@@ -10,35 +20,35 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var uid = "";
+var uid = ""; */
 //-----------------AUTENTICACIÓN DE EMAIL---------------------//
-$('#logIn').click(function() {
+/* $('#logIn').click(function() {
   const email = $('#emailLogin').val();
   const pass = $('#passwordLogin').val();
   const auth = firebase.auth();
   $('#emailLogin').val("");
   $('#passwordLogin').val("");
-  // Sign in
+  
   var promise = auth.signInWithEmailAndPassword(email, pass)
     .then(function(user) {
       console.log(user);
     })
     .catch(e => console.log(e.message));
 });
-
-$('#register').click(function() {
+ */
+/* $('#register').click(function() {
   var email = $('#emailRegister').val();
   var pass = $('#passwordRegister').val();
   var auth = firebase.auth();
   $('#emailRegister').val("");
   $('#passwordRegister').val("");
-  // Sign in
+ 
   var promise = auth.createUserWithEmailAndPassword(email, pass)
     .then(function(user) {
       console.log(user);
     })
     .catch(function(error) {
-      // Handle Errors here.
+      
       const errorCode = error.code;
       const errorMessage = error.message;
       if (errorCode == 'auth/weak-password') {
@@ -48,24 +58,24 @@ $('#register').click(function() {
       }
       console.log(error);
     });
-});
-$('#logOut').click(function() {
+}); */
+/* $('#logOut').click(function() {
   firebase.auth().signOut();
   uid = "";
-});
+}); */
 
 //Se mantiene escuchando cualquier cambio en el estado del usuario en tiempo real
-firebase.auth().onAuthStateChanged(firebaseUser => {
+/* firebase.auth().onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
     console.log(firebaseUser);
     uid = firebaseUser.uid;
     start();
   } else {
     console.log('no logueado');
-    stop();
-    //$('#logOut').classList.add('hide');
-  }
-});
+    stop(); */
+//$('#logOut').classList.add('hide');
+/*   }
+}); */
 //----------------ANIMACIÓN LOGIN/REGISTER--------------------//
 $('#login-form-link').click(function(e) {
   $("#login-form").delay(100).fadeIn(100);
@@ -86,10 +96,72 @@ function start() {
   //$('#formLogin').addClass('hide');
 }
 
-/* fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
-  .then((response) => {
-    return response.json();
+//------------------CONSULTAS A LA API THECOCKTAILDB POR FILTRO-------------------//
+let selectFilter;
+let strSelected;
+
+function fetchByFilter(filter) {
+  fetch(`http://www.thecocktaildb.com/api/json/v1/1/list.php?${filter}=list`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      selectFilter = data.drinks;
+      searchByFilter(filter);
+    })
+    .catch((error) => {
+      console.log('Hubo un problema con la operación: ' + error.message);
+    });
+}
+
+$('.filtro').mouseover(function() {
+  let f;
+  if (this.id === 'categories') {
+    f = 'c';
+  } else if (this.id === 'glasses') {
+    f = 'g';
+  } else if (this.id === 'alcoholic') {
+    f = 'a';
+  }
+  fetchByFilter(f);
+})
+
+function searchByFilter(filter) {
+  $('.dropdown-content').empty();
+  if (filter === 'c') {
+    for (let i = 0; i < selectFilter.length; i++) {
+      $('.dropdown-content').append(
+        `<li class ="strSelect" id="${selectFilter[i].strCategory}"><a href="#!">${selectFilter[i].strCategory}</a></li>`)
+    }
+  } else if (filter === 'g') {
+    for (let i = 0; i < selectFilter.length; i++) {
+      $('.dropdown-content').append(
+        `<li class ="strSelect" id="${selectFilter[i].strGlass}><a href="#!">${selectFilter[i].strGlass}</a></li>`)
+    }
+  } else if (filter === 'a') {
+    for (let i = 0; i < selectFilter.length; i++) {
+      $('.dropdown-content').append(
+        `<li class ="strSelect" id="${selectFilter[i].strAlcoholic}><a href="#!">${selectFilter[i].strAlcoholic}</a></li>`)
+    }
+  }
+  $('.strSelect').click((e) => {
+    strSelected = e.currentTarget.id;
+    selectedFilter(filter, strSelected);
   })
-  .then((data) => {
-    console.log(data)
-  }) */
+}
+
+function selectedFilter(filter, strSelected) {
+  fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?${filter}=${strSelected}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      selectFilter = data.drinks;
+      renderCocktails();
+    })
+    .catch((error) => {
+      console.log('Hubo un problema con la operación: ' + error.message);
+    });
+}
+
+function renderCocktails() {}
